0dbd7287fc02a381e6abccaf07555ef5c9b23eb7
Aditya Narayan <narayanaditya1007@gmail.com>
2022-04-29 00:05:57 +0530

    add button added

--- app/src/main/java/com/example/bookmybook/MainActivity.java
+++ app/src/main/java/com/example/bookmybook/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.bookmybook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageButton addBook;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addBook=findViewById(R.id.imageButton_addBook);
+        addBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,addBook.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
