0336a549504a0e585baed3d6da61c080aec2e912
Tabish Malik <76650437+Dark-Hope-tech@users.noreply.github.com>
2022-04-29 05:03:20 +0530

    done

---
+++ app/src/main/java/com/example/bookmybook/Models/BookModel.java
@@ -0,0 +1,44 @@
+package com.example.bookmybook.Models;
+
+public class BookModel {
+    private String name,author,Description;
+    private int BookID;
+    public BookModel(String name, String author, String description, int bookID) {
+        this.name = name;
+        this.author = author;
+        Description = description;
+        BookID = bookID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public int getBookID() {
+        return BookID;
+    }
+
+    public void setBookID(int bookID) {
+        BookID = bookID;
+    }
+}
---
+++ app/src/main/java/com/example/bookmybook/Models/IssueModel.java
@@ -0,0 +1,55 @@
+package com.example.bookmybook.Models;
+
+public class IssueModel {
+    private String UserEmail,IssueDate,ReturnDate;
+    private int BookID,issueID;
+
+    public IssueModel() { ReturnDate=null;}
+    public IssueModel(String userEmail, String issueDate, String returnDate, int bookID, int issueID) {
+        UserEmail = userEmail;
+        IssueDate = issueDate;
+        ReturnDate = returnDate;
+        BookID = bookID;
+        this.issueID = issueID;
+    }
+
+    public void setUserEmail(String userEmail) {
+        UserEmail = userEmail;
+    }
+
+    public void setIssueDate(String issueDate) {
+        IssueDate = issueDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        ReturnDate = returnDate;
+    }
+
+    public void setBookID(int bookID) {
+        BookID = bookID;
+    }
+
+    public void setIssueID(int issueID) {
+        this.issueID = issueID;
+    }
+
+    public String getUserEmail() {
+        return UserEmail;
+    }
+
+    public String getIssueDate() {
+        return IssueDate;
+    }
+
+    public String getReturnDate() {
+        return ReturnDate;
+    }
+
+    public int getBookID() {
+        return BookID;
+    }
+
+    public int getIssueID() {
+        return issueID;
+    }
+}
