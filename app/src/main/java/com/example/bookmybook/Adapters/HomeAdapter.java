c68c08aecb0fc2555878ae02a472432dd0a0c057
Tabish Malik <76650437+Dark-Hope-tech@users.noreply.github.com>
2022-05-01 05:50:43 +0530

    ok

--- app/src/main/java/com/example/bookmybook/Adapters/HomeAdapter.java
+++ app/src/main/java/com/example/bookmybook/Adapters/HomeAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -43,7 +44,7 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.viewholder> {
         holder.rent_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // yaha karo
+                Toast.makeText(context, "Successfully rented", Toast.LENGTH_SHORT).show();
             }
         });
 //        holder.book_img.setOnClickListener(new View.OnClickListener() {
